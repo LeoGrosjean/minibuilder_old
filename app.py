122fc703acdf18df9ae56f0580ef8aa562109c5a
Leo Grosjean <leo.grosjean@gmail.com>
2021-11-24 09:06:44 +0100

    wip

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ app.register_blueprint(thingidl_bp)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY_FLASK') or 'coucou_c_moi_leo'
 
+
 def main():
     # The reloader has not yet run - open the browser
     if not os.environ.get("WERKZEUG_RUN_MAIN"):
         webbrowser.open_new('http://127.0.0.1:2000/')
 
     # Otherwise, continue as normal
-    app.run(host="127.0.0.1", port=2000, debug=True)
+    app.run(host="127.0.0.1", port=2000, debug=False)
 
 
 if __name__ == '__main__':
